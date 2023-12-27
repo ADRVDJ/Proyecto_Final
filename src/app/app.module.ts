5ad2460d8ef6ffe8f1a24225c569d1b30c307920
¨Maikol <maikoltj11@hotmail.es>
2023-12-26 19:52:04 -0500

    Maikol
    Creación de interfaces listar-cursos y listar-roles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from 'src/login/login.component';
 import { SlidebarComponent } from 'src/slidebar/slidebar.component';
 import { FooterComponent } from 'src/footer/footer.component';
+import { ListarCursosComponent } from 'src/listar-cursos/listar-cursos.component';
+import { ListarRolesComponent } from 'src/listar-roles/listar-roles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SlidebarComponent,
-    FooterComponent
-   
+    FooterComponent,
+    ListarCursosComponent,
+    ListarRolesComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
