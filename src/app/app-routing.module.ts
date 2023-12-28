e881a4470a4d893073d1b63fd4eb2f71132171b3
Damian2340842 <damianparedes842@gmail.com>
2023-12-27 21:47:42 -0500

    damian
    menu desplegable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'src/login/login.component';
 import { FooterComponent } from 'src/footer/footer.component';
-
+import { ListarRolesComponent } from 'src/listar-roles/listar-roles.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path: 'footer', component: FooterComponent},
+  { path: 'listarroles', component: ListarRolesComponent },
 ];
 
 @NgModule({
