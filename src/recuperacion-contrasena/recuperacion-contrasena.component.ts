f6268f35a027a8c87ce540530d7003423ce8348b
Damian2340842 <damianparedes842@gmail.com>
2024-01-02 15:29:39 -0500

    damian
    recuparer contraseña

---
+++ src/recuperacion-contrasena/recuperacion-contrasena.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperacion-contrasena',
+  templateUrl: './recuperacion-contrasena.component.html',
+  styleUrls: ['./recuperacion-contrasena.component.css']
+})
+export class RecuperacionContrasenaComponent {
+
+}
