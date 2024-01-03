09c38b36daea76a636ff268ac5eaa1f48abd24cd
Damian2340842 <damianparedes842@gmail.com>
2024-01-02 20:59:14 -0500

    damian
    intenrto de conexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,16 @@ import { LoginComponent } from 'src/login/login.component';
 import { FooterComponent } from 'src/footer/footer.component';
 import { ListarRolesComponent } from 'src/listar-roles/listar-roles.component';
 import { PeriodosComponent } from 'src/periodos/periodos.component';
+import { SistemaAdministrativoComponent } from 'src/sistema-administrativo/sistema-administrativo.component';
 const routes: Routes = [
-  // { path: 'login', component: LoginComponent },
-  // {path: 'footer', component: FooterComponent},
-  // { path: 'listarroles', component: ListarRolesComponent },
+   { path: 'login', component: LoginComponent },
+   {path: 'footer', component: FooterComponent},
+   { path: 'listarroles', component: ListarRolesComponent },
+   {path :'sistema_administrativo', component:SistemaAdministrativoComponent}, 
 ];
 
 @NgModule({
+  
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
