e30606b967456e778288352e82f3e288798b482e
Daisy Lalvay <153593753+tatianalvy@users.noreply.github.com>
2024-01-06 01:36:27 -0500

    Daisy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
-import { ListarRolesComponent } from './listar-roles/listar-roles.component';
-import { ListadoPeriodosComponent } from './listado-periodos/listado-periodos.component';
+import { ListarRolesComponent } from './Roles/listar-roles/listar-roles.component';
+import { ListadoPeriodosComponent } from './Periodo/listado-periodos/listado-periodos.component';
 import { SistemaAdministrativoComponent } from './sistema-administrativo/sistema-administrativo.component';
 import { SlidebarComponent } from './slidebar/slidebar.component';
 const routes: Routes = [
