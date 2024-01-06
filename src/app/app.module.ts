6e3ba497587a9c789639c1fc1fb7a3f9063517e5
¨Maikol <maikoltj11@hotmail.es>
2024-01-05 22:54:33 -0500

    Maikol
    Creación de carpetas para organizar los diferentes componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,55 +1,54 @@
 // app.module.ts
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SlidebarComponent } from './slidebar/slidebar.component';
-import { FooterComponent } from './footer/footer.component';
-import { ListarCursosComponent } from './listar-cursos/listar-cursos.component';
-import { ListarRolesComponent } from './listar-roles/listar-roles.component';
+// import { RouterModule, Routes } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { LoginComponent } from './login/login.component';
+// import { SlidebarComponent } from './slidebar/slidebar.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { ListarCursosComponent } from './listar-cursos/listar-cursos.component';
+// import { ListarRolesComponent } from './listar-roles/listar-roles.component';
 //
-import { CrearActividadComponent } from './crear-actividad/crear-actividad.component';
-import { CursoComponent } from './curso/curso.component';
-import { HeaderComponent } from './header/header.component';
-import { ListadoActividadesComponent } from './listado-actividades/listado-actividades.component';
-import { ListadoCargoComponent } from './listado-cargo/listado-cargo.component';
-import { ListadoCarrerasComponent } from './listado-carreras/listado-carreras.component';
-import { MenuComponent } from './menu/menu.component';
-import { PanelCrearCarreraComponent } from './panel-crear-carrera/panel-crear-carrera.component';
-import { PanelCrearMateriasComponent } from './panel-crear-materias/panel-crear-materias.component';
-import { PeriodosComponent } from './periodos/periodos.component';
-import { RolComponent } from './rol/rol.component';
-import { ListadoPeriodosComponent } from './listado-periodos/listado-periodos.component';
-import { ListadoDocentesComponent } from './listado-docentes/listado-docentes.component';
+// import { CrearActividadComponent } from './crear-actividad/crear-actividad.component';
+// import { CursoComponent } from './curso/curso.component';
+// import { HeaderComponent } from './header/header.component';
+// import { ListadoActividadesComponent } from './listado-actividades/listado-actividades.component';
+// import { ListadoCargoComponent } from './listado-cargo/listado-cargo.component';
+// import { ListadoCarrerasComponent } from './listado-carreras/listado-carreras.component';
+// import { MenuComponent } from './menu/menu.component';
+// import { PanelCrearCarreraComponent } from './panel-crear-carrera/panel-crear-carrera.component';
+// import { PanelCrearMateriasComponent } from './panel-crear-materias/panel-crear-materias.component';
+// import { PeriodosComponent } from './periodos/periodos.component';
+// import { RolComponent } from './rol/rol.component';
+// import { ListadoPeriodosComponent } from './listado-periodos/listado-periodos.component';
+// import { ListadoDocentesComponent } from './listado-docentes/listado-docentes.component';
 
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LoginComponent,
+//     SlidebarComponent,
+//     FooterComponent,
+//     ListarCursosComponent,
+//     ListarRolesComponent,
+//     CrearActividadComponent,
+//     CursoComponent,
+//     HeaderComponent,
+//     ListadoActividadesComponent,
+//     ListadoCargoComponent,
+//     ListadoCarrerasComponent,
+//     MenuComponent,
+//     PanelCrearCarreraComponent,
+//     PanelCrearMateriasComponent,
+//     PeriodosComponent,
+//     RolComponent,
+//     ListadoPeriodosComponent,
+//     ListadoDocentesComponent,
+//   ],
+//   imports: [BrowserModule, AppRoutingModule],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SlidebarComponent,
-    FooterComponent,
-    ListarCursosComponent,
-    ListarRolesComponent,
-    CrearActividadComponent,
-    CursoComponent,
-    HeaderComponent,
-    ListadoActividadesComponent,
-    ListadoCargoComponent,
-    ListadoCarrerasComponent,
-    MenuComponent,
-    PanelCrearCarreraComponent,
-    PanelCrearMateriasComponent,
-    PeriodosComponent,
-    RolComponent,
-    ListadoPeriodosComponent,
-    ListadoDocentesComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
-})
-
-export class AppModule {}
+// export class AppModule {}
