b6afecc2185321133b69bafcb4894351814eb372
ADRVDJ <adrian.cajamarca.est@tecazuay.edu.ec>
2024-01-07 22:00:18 -0500

    asd
    com

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,28 @@
-// // app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListarRolesComponent } from './Roles/listar-roles/listar-roles.component';
+import { ListadoPeriodosComponent } from './Periodo/listado-periodos/listado-periodos.component';
+import { SistemaAdministrativoComponent } from './sistema-administrativo/sistema-administrativo.component';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { RolComponent } from './Roles/rol/rol.component';
+import { MenuComponent } from './menu/menu.component';
 
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { LoginComponent } from './login/login.component';
-// import { FooterComponent } from './footer/footer.component';
-// import { ListarRolesComponent } from './Roles/listar-roles/listar-roles.component';
-// import { ListadoPeriodosComponent } from './Periodo/listado-periodos/listado-periodos.component';
-// import { SistemaAdministrativoComponent } from './sistema-administrativo/sistema-administrativo.component';
-// import { SlidebarComponent } from './slidebar/slidebar.component';
-// const routes: Routes = [
-//   { path: 'login', component: LoginComponent },
-//   { path: 'footer', component: FooterComponent },
-//   { path: 'listarroles', component: ListarRolesComponent },
-//   { path: 'sistema_administrativo', component: SistemaAdministrativoComponent },
-//   { path: 'slidebar', component: SlidebarComponent },
-// ];
 
-// @NgModule({
-//   declarations: [
-//     // ...otros componentes
-//   ],
+//rutas navegacion
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'listarroles', component: ListarRolesComponent },
+  { path: 'sistema_administrativo', component: SistemaAdministrativoComponent },
+  { path: 'slidebar', component: SlidebarComponent },
+  { path: 'rol', component: RolComponent },
+  //{ path: 'menu', component: MenuComponent },
+];
 
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule],
-// })
-// export class AppRoutingModule {}
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  
+  constructor(private router: Router) {}
+  menuabierto = false;
+  comprobar(){
+    this.menuabierto = !this.menuabierto;
+  }
+  verificar(){
+    const rutas = this.router.url;
+    this.menuabierto = rutas.includes('rol');
+  }
+
+  navigateToRoles(): void {
+    this.router.navigate(['/roles']);
+  }
+
+  navigateToCursos(): void {
+    this.router.navigate(['/cursos']);
+  }
+
+  navigateToPeriodos(): void {
+    this.router.navigate(['/periodos']);
+  }
+
 
 }
